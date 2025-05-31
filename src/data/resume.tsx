fb311e2672576aa688d624daea635a9e8243841c
Matin-Sangabi <sangabiwork@gmail.com>
2025-05-31 22:23:41 +0330

    update readme

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -4,7 +4,7 @@ import { HomeIcon, NotebookIcon } from "lucide-react";
 export const DATA = {
   name: "Matin Sangabi",
   initials: "DV",
-  url: "https://dillion.io",
+  url: "https://matin-sangabi.io",
   location: "San Francisco, CA",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
